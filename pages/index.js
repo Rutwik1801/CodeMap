406d08c53393f86228415da16efe62d109f3f38a
k0hei2025 <vaibhavvermaonline@gmail.com>
2021-10-16 17:42:15 +0530

    add

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react'
+import Box from '../components/Box'
+import styles from '../styles/Home.module.css'
+import NavigationBar from '../components/NavigationBar/Navigation.jsx'
+function WorkSpace() {
+  return (
+    
+    <div className={styles.frame} >
+   <NavigationBar />
+    
+    
+    
+    </div> 
+  )
+}
+
+export default WorkSpace
